801e3e0d005dfcfe54f2fe43efa37a7dd8992f18
MarcusJones <jones.0bj3@gmail.com>
2018-05-26 19:08:38 +0200

    add: random choice in flask, client testing

--- Flask_jsonify/app.py
+++ Flask_jsonify/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask import jsonify
 import json
+import numpy as np
+import random
+
 
 app = Flask(__name__)
 
@@ -10,7 +13,30 @@ def index():
 
 @app.route('/sentiment')
 def sentiment():
-    data = [0.3, 0.5, 0.9]
+    data = [
+        np.random.uniform(),
+        np.random.uniform(),
+        np.random.uniform(),
+        ]
+
+    #data = [0.3, 0.5, 0.9]
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/emoji')
+def emoji():
+    
+
+    emotions = ['Happy! :)', 'Sad :*(', 'ANGRY!!! >:(']
+    #print(random.choice(foo))
+    
+    data = random.choice(emotions)
+    
     response = app.response_class(
         response=json.dumps(data),
         status=200,
