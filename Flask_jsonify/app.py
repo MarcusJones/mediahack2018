a1313855bf14f75c6c0619f91ff66d3056bc62aa
MarcusJones <jones.0bj3@gmail.com>
2018-05-27 12:21:11 +0200

    debug:

--- Flask_jsonify/app.py
+++ Flask_jsonify/app.py
@@ -20,14 +20,10 @@ import pyglet
 
 
 
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')
-
-
-
-
+#from OpenSSL import SSL
+#context = SSL.Context(SSL.SSLv23_METHOD)
+#context.use_privatekey_file('server.key')
+#context.use_certificate_file('server.crt')
 
 
 
