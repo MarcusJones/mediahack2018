b9dd52680cd1dfa81706891776e3c63d66939aa8
MarcusJones <jones.0bj3@gmail.com>
2018-05-27 12:30:01 +0200

    debug: Revert back to http!

--- Flask_jsonify/app.py
+++ Flask_jsonify/app.py
@@ -337,4 +337,6 @@ def soundpush():
 
 
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
+        #app.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
+        
+        app.run(host='0.0.0.0', debug=True)
