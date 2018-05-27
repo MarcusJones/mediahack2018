a20fd5937ceb170c58fafc2ebb44ff2450035592
MarcusJones <jones.0bj3@gmail.com>
2018-05-27 10:44:44 +0200

    add: predict testing

--- Flask_jsonify/app.py
+++ Flask_jsonify/app.py
@@ -10,6 +10,11 @@ import datetime
 from contextlib import suppress
 #import glob
 import re
+from keras.models import load_model
+import cv2
+import librosa
+from skimage.transform import resize
+
 #from astropy._erfa.core import ld
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(funcName)25s() %(levelname)-9s %(message)s',
@@ -17,12 +22,18 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 import operator
 import jsonpickle
-#import numpy as np
-#import cv2
-
 from flask_cors import CORS
 
 logging.debug("Started logging")
+path_wts = os.path.join("./SERVER MODELS", "melspectrogram_with_dropouts.h5")
+assert os.path.exists(path_wts)
+
+logging.info("Loading model from {}".format(path_wts))
+    
+MODEL = load_model(path_wts)
+
+logging.info("Loaded model {}".format(MODEL))
+MODEL.summary()
 
 # Create app
 app = Flask(__name__)
@@ -30,11 +41,25 @@ app = Flask(__name__)
 # Enable Cross Origin Resource Sharing
 CORS(app)
 
+# EMOJI_MAP = {
+#         'happy'     :   'Happy! :)     http://url_happy',
+#         'sad'       :   'Sad :*(     http://url_happy',
+#         'neutral'   :   '-neutral- :| http://url_neutral',
+#         'angry'     :   'ANGRY!!! >:(     http://url_happy',
+#     }
+
 EMOJI_MAP = {
-        'happy'     :   'Happy! :)     http://url_happy',
-        'sad'       :   'Sad :*(     http://url_happy',
-        'neutral'   :   '-neutral- :| http://url_neutral',
-        'angry'     :   'ANGRY!!! >:(     http://url_happy',
+        'happy'     :   'happy'  ,
+        'sad'       :   'sad'    ,
+        'neutral'   :   'neutral',
+        'angry'     :   'angry'  ,
+    }
+
+TWEET_MAP = {
+        'happy'     :   "I love hacking media! #hacking #media"  ,
+        'sad'       :   "Why am I here, this code doesn't compile! #sad #demotivated"    ,
+        'neutral'   :   "Hello from Media Hackday, I wish I had more sleep #sleepy #sheep",
+        'angry'     :   "I hate hacking, I hate Media. #fu #media"  ,
     }
 
 @app.route("/")
@@ -116,7 +141,7 @@ def imagepush():
 def reload_audio(folder_path):
     logging.info("Retrieving audio from {}".format(folder_path))
     audio_files = get_files_by_name_ext(folder_path,'audio','audio')
-
+    
     dated_list = list()
     for f in audio_files:
         fname, _ = os.path.splitext(f)
@@ -146,16 +171,25 @@ def reload_audio(folder_path):
     with open(path_most_recent, "rb") as f:
         bdata = f.read(blocksize)
     logging.info("{} kB loaded".format(len(bdata)/1000))
-
     
     #np_bdata = np.frombuffer(bdata,dtype=np.uint8)
-    np_bdata = np.frombuffer(bdata,dtype=np.float64)
-    print(np_bdata)
+    byte_type = np.uint8
+    np_bdata = np.frombuffer(bdata,dtype=byte_type)
+    logging.debug("Returning raw data as numpy array {}: {}".format(byte_type,np_bdata))
+        
+    return np_bdata
+
+def get_log_melspectrum(data, sample_rate=48000):
+    melspectrum = librosa.feature.melspectrogram(data, sample_rate)
+    log_melspec = librosa.power_to_db(melspectrum, ref=np.max)
+    scaled_spec = 1.0 - np.divide(log_melspec, -80.0)
+    logging.debug("Processed log mel spectrum".format())    
+    return resize(scaled_spec, (128, 128)).reshape(128, 128, 1)
+
+
+def process_audio(audio_array):
+    #processed_data = get_log_melspectrum(audio_array)
     
-    return bdata
-
-def process_audio(fpath):
-
     # Load the file
     #TODO: This is my todo.
     logging.info("Loaded {}".format(fpath))
@@ -170,7 +204,7 @@ def get_files_by_name_ext(folder_path, search_name, search_ext):
     # Filter
     all_files = os.listdir(folder_path)
     filtered_file_list = [f for f in os.listdir(folder_path) if re.match(r'^audio.*\.audio', f)]
-
+    
     #print(filtered_file_list)
     logging.info("Found {} {} files matching '{}' in {}, out of {} total files".format(len(filtered_file_list),
                                                                            search_ext,
@@ -183,15 +217,18 @@ def get_files_by_name_ext(folder_path, search_name, search_ext):
 
 def predict(audio_array):
     logging.info("Predicting on array {}".format(audio_array.shape))
-
+    
 def save_file(file_object):
     currtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    path_audio_file_saved = os.path.join('./SERVER INCOMING', 'audio'+currtime)
-    try:
-        file.save(path_audio_file_saved)
-        logging.info("File saved; {}".format(path_audio_file_saved))
-    except:
-        logging.error("MOCK File saved; {} MOCK".format(path_audio_file_saved))
+    path_audio_file_saved = os.path.join('./SERVER INCOMING', 'audio'+currtime+'.audio')
+    file_object.save(path_audio_file_saved)
+    logging.info("File saved; {}".format(path_audio_file_saved))
+        
+#     try:
+#         file.save(path_audio_file_saved)
+#         logging.info("File saved; {}".format(path_audio_file_saved))
+#     except:
+#         logging.error("MOCK File saved; {} MOCK".format(path_audio_file_saved))
 
     return
 
@@ -199,10 +236,6 @@ def get_model(path_wts):
     """
     Model architecture
     """
-    logging.info("Loading model from {}".format(path_wts))
-    
-    model = load_model(path_wts)
-
     
     #logging.info("Intantiated model".format())
     #logging.info("Applied model weights".format())
@@ -230,15 +263,24 @@ def soundpush():
         logging.info('files attrib: {}'.format(r.files))
 
     #------------------------------------------------------------------ Get file
-    if 'data' in r.files:
-        file = r.files['data']
+    if 'audioBlob' in r.files:
+        file = r.files['audioBlob']
     else:
         logging.error("****** NO ['data' attribute in r.files!".format())
         file = 'MOCK'
-
-
+    
+    #-------------------------------------------------- Can ignore the form data
+    if 'audioBlob' in r.form:
+        form_data = r.form['audioChunks']
+    else:
+        pass
+    
     #----------------------------------------------------------------- Save file
-    save_file(file)
+    if 'audioBlob' in r.files:    
+        save_file(file)
+    else:
+        logging.error("MOCK No data to save".format())
+        
 
     #-------------------------------------------------------------------- Reload
     audio_data = reload_audio("./SERVER INCOMING")
@@ -253,13 +295,18 @@ def soundpush():
 
     #-------------------------------------------------------------- Return emoji
     this_emoji = emoji_from_senti(senti_vector)
+    this_tweet = TWEET_MAP[this_emoji]
     
     #------------------------------------------------------------ Build response
-    response = {'message': 'audio file recieved', 'emoji':this_emoji}
-
+    response = {
+        'message': 'audio file recieved',
+        'emoji':this_emoji,
+        'tweet':this_tweet,
+                }
+    print(response)
     #----------------------------------------------------------- Return response
     response_pickled = jsonpickle.encode(response)
-
+    
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
